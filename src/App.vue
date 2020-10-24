4bfb4e6e1645d96d245bf71f6a3711299673fe5b
Anthony <antholord@hotmail.com>
2020-10-23 21:34:09 -0400

    update packages

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="TOTOTTO"/>
+    <img
+      alt="Vue logo"
+      src="./assets/logo.png"
+    >
+    <HelloWorld msg="TOTOTTO" />
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import Vue from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default Vue.extend({
-  name: 'App',
+  name: "App",
   components: {
     HelloWorld
   }
-})
+});
 </script>
 
 <style>
