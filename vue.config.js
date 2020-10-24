60e5890c6ad54f09b9ed194ef15d1db3bb88de4d
Anthony <antholord@hotmail.com>
2020-10-23 22:23:17 -0400

    add config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   pluginOptions: {
+    entry: {
+      app: [
+        "./src/main.js"
+      ]
+    },
     electronBuilder: {
       nodeIntegration: true,
       builderOptions: {
