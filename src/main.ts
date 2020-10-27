f5723b7af363dca023cd97cb18f6d0007dc0889e
Anthony <antholord@hotmail.com>
2020-10-27 18:03:09 -0400

    Revert "add vuetify"
    This reverts commit 43d82ee4c3889803749c4d21e26ec2d05a18e62e.

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,10 @@ import Vue from "vue";
 import App from "./App.vue";
 
 import VueElectron from "vue-electron";
-import vuetify from "./plugins/vuetify";
-import "roboto-fontface/css/roboto/roboto-fontface.css";
-import "@mdi/font/css/materialdesignicons.css";
 
 Vue.config.productionTip = false;
 Vue.use(VueElectron as any);
 
 new Vue({
-  vuetify,
   render: h => h(App)
 }).$mount("#app");
